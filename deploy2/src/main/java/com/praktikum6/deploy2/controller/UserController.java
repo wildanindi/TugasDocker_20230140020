066782a3c130172fa3db5e1dc98f2e260587555c
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-04-14 14:41:51 +0700

    update UserController

--- deploy2/src/main/java/com/praktikum6/deploy2/controller/UserController.java
+++ deploy2/src/main/java/com/praktikum6/deploy2/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.praktikum6.deploy2.controller;
 
-
+import com.praktikum6.deploy2.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
     private final String USERNAME = "admin";
     private final String PASSWORD = "20230140020";
+
+    private List<User> userList = new ArrayList<>();
 
 
     @GetMapping("/")
@@ -23,15 +28,32 @@ public class UserController {
                          @RequestParam String password,
                          Model model) {
         if (USERNAME.equals(username) && PASSWORD.equals(password)) {
-            return "home";
+            return "redirect:/home";
         } else {
             model.addAttribute("error", "Invalid username or password");
             return "login";
         }
     }
     @GetMapping("/home")
-    public String homepage(){
+    public String homepage(Model model){
+        model.addAttribute("users", userList);
         return "home";
+    }
+
+    @GetMapping("/input-data")
+    public String tampilkanForm() {
+        return "form";
+    }
+
+    @PostMapping("/submit-data")
+    public String prosesDataMahasiswa(@RequestParam String nama,
+                                      @RequestParam String nim,
+                                      @RequestParam("jenis_kelamin") String jenisKelamin) {
+        User newUser = new User(nama, nim, jenisKelamin);
+
+        userList.add(newUser);
+
+        return "redirect:/home";
     }
 }
 
