a64f7c549f12ada411e634f61899ff4804ca04a0
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-04-14 14:01:32 +0700

    Menambah UserController untuk data user dan logika untuk login

--- deploy2/src/main/java/com/praktikum6/deploy2/controller/UserController.java
+++ deploy2/src/main/java/com/praktikum6/deploy2/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.praktikum6.deploy2.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "20230140020";
+
+
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login (@RequestParam String username,
+                         @RequestParam String password,
+                         Model model) {
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            return "home";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
+    }
+    @GetMapping("/home")
+    public String homepage(){
+        return "home";
+    }
 }
+
